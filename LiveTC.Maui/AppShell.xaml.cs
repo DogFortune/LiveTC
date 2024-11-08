e50f6f0c436151ff9135b635864d7a35ffec56c1
DogFortune <dogfortune.2000@gmail.com>
2024-11-08 22:10:55 +0900

    init

---
+++ LiveTC.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LiveTC.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ LiveTC.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LiveTC.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
